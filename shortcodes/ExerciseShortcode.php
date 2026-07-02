de411444094d2ae10ad176a43e8916d63a7b5bb9
Paul Hibbitts 🍁 <paul@paulhibbitts.com>
2026-07-02 13:31:51 -0700

    Render full body content in exercise shortcode when block elements are present.

--- shortcodes/ExerciseShortcode.php
+++ shortcodes/ExerciseShortcode.php
@@ -19,8 +19,9 @@ class ExerciseShortcode extends Shortcode
             $iconUri = 'plugin://github-markdown-alerts/assets/icons/octicon-tip.svg';
             $icon    = GravExtension::svgImageFunction($iconUri);
 
-            // Render the first link in the content as a prominent button
-            if (preg_match('/<a\s[^>]*href=["\']([^"\']+)["\'][^>]*>/i', $content, $match)) {
+            // Compact button mode: only when content is a bare link with no surrounding block elements
+            $hasRichContent = (bool) preg_match('/<(?:p|ul|ol|blockquote|details|h[1-6])\b/i', $content);
+            if (!$hasRichContent && preg_match('/<a\s[^>]*href=["\']([^"\']+)["\'][^>]*>/i', $content, $match)) {
                 $href = htmlspecialchars(html_entity_decode($match[1], ENT_QUOTES, 'UTF-8'), ENT_QUOTES, 'UTF-8');
                 $body = '<p><a class="hor-h5p-btn" href="' . $href . '" target="_blank" rel="noopener">Open Interactive Activity ↗</a></p>';
             } else {
